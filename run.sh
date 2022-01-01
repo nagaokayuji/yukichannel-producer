f1e33c57ad03e0dccb663b84a4be97ef5c6279d2
Yuji Nagaoka <yvay5cqe@gmail.com>
2022-01-01 21:09:13 +0900

    command

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 ! videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! x264enc bframes=0 key-int-max=45 bitrate=500 tune=zerolatency  ! h264parse ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline ! kvssink stream-name=$KVS_STREAM_NAME access-key=$AWS_ACCESS_KEY secret-key=$AWS_SECRET_ACCESS_KEY aws-region=$AWS_REGION
